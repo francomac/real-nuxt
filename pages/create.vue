39d6a51fcd24b623b971767987f9516e857c852b
francomac <franco.mcarballo@outlook.com>
2022-03-01 21:13:47 -0600

    add meta data for create page

--- pages/create.vue
+++ pages/create.vue
@@ -6,6 +6,20 @@
 
 <script>
 export default {
-  name: 'CreateEvent'
+  name: 'CreateEvent',
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:  'where you can create an event taking place'
+
+        }
+
+      ]
+    }
+  }
 }
 </script>

234e31800b8e59c531453d065c5f1912ea0be81e
Franco Carballo <franco.mcarballo@outlook.com>
2022-03-02 21:57:35 -0600

    new event details main page

---
+++ pages/event/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Event Details Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventDetails',
+  head() {
+    return {
+      title: 'Event Details Page',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'All you need to know about Event Details',
+        },
+      ],
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    },
+  },
+}
+</script>
+
+<style></style>

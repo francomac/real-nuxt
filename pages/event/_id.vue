d5bbfdf626605a2adee530a567525e09b7804b06
Franco Carballo <franco.mcarballo@outlook.com>
2022-03-02 21:57:20 -0600

    new event by id route

---
+++ pages/event/_id.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Event #{{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventDetails',
+  head() {
+    return {
+      title: 'Event #' +  this.id,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'All you need to know about event #' + this.id,
+        },
+      ],
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    },
+  },
+}
+</script>
+
+<style></style>

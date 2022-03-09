ad328af9a80d0d901ac13f126da3c1f729f28b32
Franco Carballo <franco.mcarballo@outlook.com>
2022-03-08 22:45:09 -0600

    use eventCard component

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
     <h1>Events</h1>
+        <EventCard
+          v-for="(event, index) in events"
+          :key="index"
+          :event="event"
+          :data-index="index"
+        />
   </div>
 </template>
 
 <script>
+ import EventCard from '@/components/EventCard.vue'
 export default {
   name: 'IndexPage',
+  components: {
+    EventCard
+  },
+  asyncData(context) {
+    return context.$axios.get('http://localhost:3001/events').then((response) => {
+      return {
+        events: response.data,
+      }
+    })
+  },
   head() {
     return {
       title: 'Event Listing',

d458e6ebff56263968ab4b2f2de0d3a85572e54d
Franco Carballo <franco.mcarballo@outlook.com>
2022-03-08 22:50:23 -0600

    error handling for nuxt/axios call

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,41 @@
 <template>
   <div>
     <h1>Events</h1>
-        <EventCard
-          v-for="(event, index) in events"
-          :key="index"
-          :event="event"
-          :data-index="index"
-        />
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
- import EventCard from '@/components/EventCard.vue'
+import EventCard from '@/components/EventCard.vue'
 export default {
   name: 'IndexPage',
   components: {
-    EventCard
+    EventCard,
   },
-  asyncData(context) {
-    return context.$axios.get('http://localhost:3001/events').then((response) => {
-      return {
-        events: response.data,
-      }
-    })
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3001/events')
+      .then((response) => {
+        return {
+          events: response.data,
+        }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again.',
+        })
+      })
   },
   head() {
     return {
       title: 'Event Listing',
     }
-  }
+  },
 }
 </script>

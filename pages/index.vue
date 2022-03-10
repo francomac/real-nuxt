29a0edd22b5685f90df17319678cf68ce806a7c4
Franco Carballo <franco.mcarballo@outlook.com>
2022-03-09 22:24:50 -0600

    use async/await instead

--- pages/index.vue
+++ pages/index.vue
@@ -17,20 +17,19 @@ export default {
   components: {
     EventCard,
   },
-  asyncData({ $axios, error }) {
-    return $axios
-      .get('http://localhost:3001/events')
-      .then((response) => {
-        return {
-          events: response.data,
-        }
+  async asyncData({ $axios, error }) {
+    try {
+      const response = await $axios.get('http://localhost:3001/events')
+
+      return {
+        events: response.data,
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time. Please try again.',
       })
-      .catch((e) => {
-        error({
-          statusCode: 503,
-          message: 'Unable to fetch events at this time. Please try again.',
-        })
-      })
+    }
   },
   head() {
     return {
